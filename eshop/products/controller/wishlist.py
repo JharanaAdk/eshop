e410c9e0b45d8fda9873948d2efdef9be1c9251f
Jharana <103406836+JharanaAdk@users.noreply.github.com>
2023-02-14 18:43:32 +0545

    wish

--- eshop/products/controller/wishlist.py
+++ eshop/products/controller/wishlist.py
@@ -9,3 +9,37 @@ def index(request):
     wishlist = Wishlist.objects.filter(user=request.user)
     context = {'wishlist':wishlist}
     return render(request, 'product/wishlist.html', context)
+
+def addtowishlist(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            prod_id = int(request.POST.get('product_id'))
+            product_check = Product.objects.get(id=prod_id) 
+            if(product_check):
+                if(Wishlist.objects.filter(user=request.user, product_id=prod_id)):
+                    return JsonResponse({'status':"Product already in wishlist"})
+                else:
+                    Wishlist.objects.create(user=request.user, product_id=prod_id)
+                    return JsonResponse({'status':"Product added to wishlist"})
+            else:
+                return JsonResponse({'status':"No such Product found"})
+        else:
+            return JsonResponse({'status':"Login to continue"})
+    return redirect('/')
+
+
+def deletewishlistitem(request):
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            prod_id = int(request.POST.get('product_id'))
+            if(Wishlist.objects.filter(user=request.user, product_id=prod_id)):
+                wishlistitem = Wishlist.objects.get(product_id=prod_id)
+                wishlistitem.delete()
+                return JsonResponse({'status':"Product removed from wishlist"})
+            else:
+                return JsonResponse({'status':"Product not found in wishlist"})
+        else:
+            return JsonResponse({'status':"Login to continue"})
+    return redirect('/')
+
+   
