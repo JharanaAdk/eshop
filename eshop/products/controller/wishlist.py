e82366f0d00d40e7d5e8f1ad32995af57c299a9b
Jharana <103406836+JharanaAdk@users.noreply.github.com>
2023-02-14 15:34:50 +0545

    wishlist

--- eshop/products/controller/wishlist.py
+++ eshop/products/controller/wishlist.py
@@ -1,1 +1,11 @@
+from django.contrib import messages
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from products.models import Product, Wishlist
 
+@login_required(login_url='loginpage')
+def index(request):
+    wishlist = Wishlist.objects.filter(user=request.user)
+    context = {'wishlist':wishlist}
+    return render(request, 'product/wishlist.html', context)
